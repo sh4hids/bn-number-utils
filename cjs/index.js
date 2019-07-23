75fbc725992eddfb1fd11b57cca1cc4c55ebc8b6
Shahidul Islam Majumder <shahidul.islam@springrainit.com>
2019-07-23 14:11:35 +0600

    Add convertToTaka method

--- cjs/index.js
+++ cjs/index.js
@@ -3,7 +3,9 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 
 var convertNumbers = require('./convertNumbers.js');
+var convertToTaka = require('./convertToTaka.js');
 
 
 
 exports.convertNumbers = convertNumbers;
+exports.convertToTaka = convertToTaka;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,2 @@
 export { default as convertNumbers } from './convert-numbers';
+export { default as convertToTaka } from './convert-to-taka';
